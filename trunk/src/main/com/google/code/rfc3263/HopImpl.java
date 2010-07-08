25704405e44ba63eee7c4d4779d1099b5932a5a1
David Grant <david@grant.org.uk>
2010-07-08 15:02:31 +0000

    Added lots of unit tests.  It appears the router is not quite as compliant as first understood!

--- trunk/src/main/com/google/code/rfc3263/HopImpl.java
+++ trunk/src/main/com/google/code/rfc3263/HopImpl.java
@@ -32,4 +32,47 @@ class HopImpl implements Hop {
 	public String toString() {
 		return host + ":" + port + "/" + transport;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + port;
+		result = prime * result
+				+ ((transport == null) ? 0 : transport.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof HopImpl)) {
+			return false;
+		}
+		HopImpl other = (HopImpl) obj;
+		if (host == null) {
+			if (other.host != null) {
+				return false;
+			}
+		} else if (!host.equals(other.host)) {
+			return false;
+		}
+		if (port != other.port) {
+			return false;
+		}
+		if (transport == null) {
+			if (other.transport != null) {
+				return false;
+			}
+		} else if (!transport.equals(other.transport)) {
+			return false;
+		}
+		return true;
+	}
 }
