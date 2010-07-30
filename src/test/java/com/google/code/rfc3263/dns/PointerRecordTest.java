f24b0113ba96e5606a58037bd73de0194493f931
David Grant <david@grant.org.uk>
2010-07-30 07:21:49 +0000

    Updated junit version, and added equals/hashCode tests.

--- src/test/java/com/google/code/rfc3263/dns/PointerRecordTest.java
+++ src/test/java/com/google/code/rfc3263/dns/PointerRecordTest.java
@@ -1,31 +1,15 @@
 package com.google.code.rfc3263.dns;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import org.junit.experimental.theories.DataPoint;
 
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
+import com.google.code.rfc3263.ObjectTest;
 
 
-public class PointerRecordTest {
-	@Ignore @Test
-	public void sortingTest() {
-		List<PointerRecord> pointers = new ArrayList<PointerRecord>();
-		
-		PointerRecord tls = new PointerRecord("example.org.", 3, 1, "s", "SIPS+D2T", "", "_sips._tcp.example.org.");
-		PointerRecord tcp = new PointerRecord("example.org.", 1, 2, "s", "SIP+D2T", "", "_sip._tcp.example.org.");
-		PointerRecord udp = new PointerRecord("example.org.", 1, 3, "s", "SIP+D2U", "", "_sip._udp.example.org.");
-		
-		pointers.add(tls);
-		pointers.add(tcp);
-		pointers.add(udp);
-		
-		Iterator<PointerRecord> iter = pointers.iterator();
-		
-		Assert.assertEquals(tcp, iter.next());
-		Assert.assertEquals(udp, iter.next());
-		Assert.assertEquals(tls, iter.next());
-	}
+public class PointerRecordTest extends ObjectTest {
+	@DataPoint
+	public static PointerRecord tls = new PointerRecord("example.org.", 3, 1, "s", "SIPS+D2T", "", "_sips._tcp.example.org.");
+	@DataPoint
+	public static PointerRecord tcp = new PointerRecord("example.org.", 1, 2, "s", "SIP+D2T", "", "_sip._tcp.example.org.");
+	@DataPoint
+	public static PointerRecord udp = new PointerRecord("example.org.", 1, 3, "s", "SIP+D2U", "", "_sip._udp.example.org.");
 }
