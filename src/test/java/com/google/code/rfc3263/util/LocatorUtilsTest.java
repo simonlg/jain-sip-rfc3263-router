cecb3c894bfa05cf4b6c11470373154446086e6f
David Grant <david@grant.org.uk>
2010-08-13 12:27:41 +0000

    Increased tests for LocatorUtils.

--- src/test/java/com/google/code/rfc3263/util/LocatorUtilsTest.java
+++ src/test/java/com/google/code/rfc3263/util/LocatorUtilsTest.java
@@ -1,6 +1,7 @@
 package com.google.code.rfc3263.util;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 import org.apache.log4j.BasicConfigurator;
@@ -59,5 +60,110 @@ public class LocatorUtilsTest {
 	@Test
 	public void testIsNumericHost() {
 		assertFalse(LocatorUtils.isNumeric("example.org"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void defaultPortForInvalidTransportThrowsException() {
+		LocatorUtils.getDefaultPortForTransport("FOO");
+	}
+	
+	@Test
+	public void defaultPortForUdpIs5060() {
+		assertEquals(5060, LocatorUtils.getDefaultPortForTransport("UDP"));
+	}
+	
+	@Test
+	public void defaultPortForTcpIs5060() {
+		assertEquals(5060, LocatorUtils.getDefaultPortForTransport("TCP"));
+	}
+	
+	@Test
+	public void defaultPortForSctpIs5060() {
+		assertEquals(5060, LocatorUtils.getDefaultPortForTransport("SCTP"));
+	}
+	
+	@Test
+	public void defaultPortForTlsIs5061() {
+		assertEquals(5061, LocatorUtils.getDefaultPortForTransport("TLS"));
+	}
+	
+	@Test
+	public void defaultPortForSctpTlsIs5061() {
+		assertEquals(5061, LocatorUtils.getDefaultPortForTransport("SCTP-TLS"));
+	}
+	
+	@Test
+	public void shouldUpgradeTcpToTls() {
+		assertEquals("TLS", LocatorUtils.upgradeTransport("TCP"));
+	}
+	
+	@Test
+	public void shouldUpgradeSctpToSctpTls() {
+		assertEquals("SCTP-TLS", LocatorUtils.upgradeTransport("SCTP"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToUpgradeUnknownTransport() {
+		LocatorUtils.upgradeTransport("FOO");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToUpgradeUdp() {
+		LocatorUtils.upgradeTransport("UDP");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToUpgradeTls() {
+		LocatorUtils.upgradeTransport("TLS");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToUpgradeSctpTls() {
+		LocatorUtils.upgradeTransport("SCTP-TLS");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void defaultTransportForInvalidSchemeThrowsException() {
+		LocatorUtils.getDefaultTransportForScheme("foo");
+	}
+
+	@Test
+	public void defaultTransportForSipIsUdp() {
+		assertEquals("UDP", LocatorUtils.getDefaultTransportForScheme("sip"));
+	}
+	
+	@Test
+	public void defaultTransportForSipsIsTls() {
+		assertEquals("TLS", LocatorUtils.getDefaultTransportForScheme("sips"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetServiceIdentifierForInvalidTransport() {
+		LocatorUtils.getServiceIdentifier("FOO", "example.org.");
+	}
+	
+	@Test
+	public void testGetServiceIdentifierForUdp() {
+		assertEquals("_sip._udp.example.org.", LocatorUtils.getServiceIdentifier("UDP", "example.org."));
+	}
+	
+	@Test
+	public void testGetServiceIdentifierForTcp() {
+		assertEquals("_sip._tcp.example.org.", LocatorUtils.getServiceIdentifier("TCP", "example.org."));
+	}
+	
+	@Test
+	public void testGetServiceIdentifierForTls() {
+		assertEquals("_sips._tcp.example.org.", LocatorUtils.getServiceIdentifier("TLS", "example.org."));
+	}
+	
+	@Test
+	public void testGetServiceIdentifierForSctp() {
+		assertEquals("_sip._sctp.example.org.", LocatorUtils.getServiceIdentifier("SCTP", "example.org."));
+	}
+	
+	@Test
+	public void testGetServiceIdentifierForSctpTls() {
+		assertEquals("_sips._sctp.example.org.", LocatorUtils.getServiceIdentifier("SCTP-TLS", "example.org."));
 	}
 }
