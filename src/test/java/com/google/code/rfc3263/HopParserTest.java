d09d6ba6cd4b52f81754076adf5e0f42ff9ba467
David Grant <david@grant.org.uk>
2010-09-08 15:54:08 +0000

    Added hop parsing tests.  Currently fail.

--- src/test/java/com/google/code/rfc3263/HopParserTest.java
+++ src/test/java/com/google/code/rfc3263/HopParserTest.java
@@ -1,10 +1,10 @@
 package com.google.code.rfc3263;
 
+import static org.junit.Assert.assertEquals;
+
 import java.text.ParseException;
 
 import javax.sip.address.Hop;
-
-import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -15,7 +15,7 @@ public class HopParserTest {
 		Hop expectedHop = new HopImpl("127.0.0.1", 5060, "TCP");
 		Hop actualHop = HopImpl.getInstance("127.0.0.1:5060/TCP");
 		
-		Assert.assertEquals(expectedHop, actualHop);
+		assertEquals(expectedHop, actualHop);
 	}
 	
 	@Test(expected = ParseException.class)
@@ -56,5 +56,59 @@ public class HopParserTest {
 	@Test(expected = ParseException.class)
 	public void testHostAddressShouldThrowException() throws ParseException {
 		HopImpl.getInstance("example.org:5060/TCP");
+	}
+	
+	@Test
+	public void testHostnameHost() throws ParseException {
+		Hop hop = HopImpl.getInstance("example.org:5060/TCP");
+		assertEquals("example.org", hop.getHost());
+	}
+	
+	@Test
+	public void testHostnamePort() throws ParseException {
+		Hop hop = HopImpl.getInstance("example.org:5060/TCP");
+		assertEquals(5060, hop.getPort());
+	}
+	
+	@Test
+	public void testHostnameTransport() throws ParseException {
+		Hop hop = HopImpl.getInstance("example.org:5060/TCP");
+		assertEquals("TCP", hop.getTransport());
+	}
+	
+	@Test
+	public void testIPv6ReferenceHost() throws ParseException {
+		Hop hop = HopImpl.getInstance("[::1]:5060/TCP");
+		assertEquals("::1", hop.getHost());
+	}
+	
+	@Test
+	public void testIPv6ReferencePort() throws ParseException {
+		Hop hop = HopImpl.getInstance("[::1]:5060/TCP");
+		assertEquals(5060, hop.getPort());
+	}
+	
+	@Test
+	public void testIPv6ReferenceTransport() throws ParseException {
+		Hop hop = HopImpl.getInstance("[::1]:5060/TCP");
+		assertEquals("TCP", hop.getTransport());
+	}
+	
+	@Test(expected=ParseException.class)
+	public void testIPv6Host() throws ParseException {
+		Hop hop = HopImpl.getInstance("::1:5060/TCP");
+		assertEquals("::1", hop.getHost());
+	}
+	
+	@Test(expected=ParseException.class)
+	public void testIPv6Port() throws ParseException {
+		Hop hop = HopImpl.getInstance("::1:5060/TCP");
+		assertEquals(5060, hop.getPort());
+	}
+	
+	@Test(expected=ParseException.class)
+	public void testIPv6Transport() throws ParseException {
+		Hop hop = HopImpl.getInstance("::1:5060/TCP");
+		assertEquals("TCP", hop.getTransport());
 	}
 }
